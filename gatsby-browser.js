301b36731c40aef4f577e62ace34d0ab9fd99dc1
taehoon02 <taevel02@gmail.com>
2019-04-12 11:01:20 +0900

    chore: environment settings)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,9 @@
-import 'typeface-nanum-gothic';
-import 'typeface-nanum-square-round';
-// import './src/scss/theme.scss';
+import 'typeface-poppins';
+import 'typeface-notosans-kor';
+
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/regular.css';
+
+import './src/scss/theme.scss';

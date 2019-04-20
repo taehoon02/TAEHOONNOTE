dddad65da52adb1065729eeb9dd48b962ef663b6
taehoon02 <taevel02@gmail.com>
2019-04-20 13:03:39 +0900

    feat: create post card component

---
+++ src/components/PostCard.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 800px;
+  max-height: 328px;
+  margin-right: -20px;
+  margin-left: -20px;
+  border-top: 1px #e9ecef solid;
+  border-bottom: 1px #e9ecef solid;
+  font-size: 4rem;
+`;
+
+const MediaWrapper = styled.div`
+  position: relative;
+  flex: 0 0 50%;
+  padding-right: 20px;
+  padding-left: 20px;
+`;
+
+const Media = styled.img`
+  position: relative;
+  display: block;
+  overflow: hidden;
+  width: 380px;
+  height: 100%;
+  margin-top: -1px;
+  background-position: center center;
+  background-size: cover;
+  text-align: center;
+`;
+
+const ContentWrapper = styled.div`
+  padding-top: 30px;
+  padding-right: 20px;
+  padding-bottom: 30px;
+  padding-left: 20px;
+`;
+
+const ContentHeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+`;
+
+const ContentHeaderCategory = styled.a`
+  display: inline-block;
+  margin-bottom: 1rem;
+  box-shadow: none;
+  color: #f73832;
+  font-family: 'Poppins', sans-serif;
+  font-size: 10px;
+  font-weight: 400;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+`;
+
+const ContentHeaderTitle = styled(Link)`
+  box-shadow: none;
+  color: #0a0a0a;
+  font-size: 1.25rem;
+  font-weight: 700;
+  letter-spacing: -0.05em;
+  text-transform: none;
+`;
+
+const ContentHeaderDate = styled.span`
+  margin-top: 1rem;
+  color: #0a0a0a;
+  font-family: 'Poppins', sans-serif;
+  font-size: 10px;
+  font-weight: 400;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+`;
+
+const ContentExcerpt = styled.div`
+  display: block;
+  margin-bottom: 1.5rem;
+  font-size: 0.9rem;
+  font-weight: 400;
+  letter-spacing: 0.05px;
+  line-height: 1.85;
+  text-align: left;
+`;
+
+class PostCard extends React.Component {
+  render() {
+    const { category, thumbnail, link, title, date, excerpt } = this.props;
+    return (
+      <Wrapper>
+        <MediaWrapper>
+          <Media src={thumbnail} />
+        </MediaWrapper>
+        <ContentWrapper>
+          <ContentHeaderWrapper>
+            <ContentHeaderCategory>{category.join(`, `)}</ContentHeaderCategory>
+            <ContentHeaderTitle to={link}>{title}</ContentHeaderTitle>
+            <ContentHeaderDate>{date}</ContentHeaderDate>
+          </ContentHeaderWrapper>
+          <ContentExcerpt>{excerpt}</ContentExcerpt>
+        </ContentWrapper>
+      </Wrapper>
+    );
+  }
+}
+
+PostCard.propTypes = {
+  category: PropTypes.arrayOf(PropTypes.string),
+  thumbnail: PropTypes.string,
+  link: PropTypes.string,
+  title: PropTypes.string,
+  date: PropTypes.string,
+  excerpt: PropTypes.string,
+};
+
+export default PostCard;

cd2add0f78cd3c263855af89dc191e3542dc47c8
taehoon02 <taevel02@gmail.com>
2019-04-28 12:40:42 +0900

    fix: completed basic layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styled from 'styled-components';
+
 import Header from './Header';
 import Navbar from './Navbar';
 import Footer from './Footer';
-
 import Instagram from './Instagram';
+
+const Wrapper = styled.div`
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 1200px;
+  flex-direction: row;
+  padding-right: 20px;
+  padding-left: 20px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+const ContentArea = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  min-width: 0;
+  max-width: 840px;
+  flex: 0 0 840px;
+  flex-direction: column;
+  flex-wrap: wrap;
+  padding-right: 20px;
+  padding-left: 20px;
+`;
+
+const WidgetArea = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  min-width: 0;
+  max-width: 360px;
+  flex: 0 0 360px;
+  flex-direction: column;
+  flex-wrap: wrap;
+  padding-right: 20px;
+  padding-left: 40px;
+`;
 
 class Layout extends React.Component {
   render() {
@@ -15,16 +58,14 @@ class Layout extends React.Component {
       <div>
         <Header />
         <Navbar />
-        <br />
-        <br />
-        <br />
-        <br />
-        <main>{children}</main>
-        <Instagram />
-        <br />
-        <br />
-        <br />
-        <br />
+        <Wrapper>
+          <Container>
+            <ContentArea>{children}</ContentArea>
+            <WidgetArea>
+              <Instagram />
+            </WidgetArea>
+          </Container>
+        </Wrapper>
         <Footer />
       </div>
     );

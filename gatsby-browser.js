95918e51a63da17979ce09e21d95061f603af7dc
taehoon02 <taevel02@gmail.com>
2019-05-13 10:19:14 +0900

    fix: add error boundary class in gatsby-brower

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as Sentry from '@sentry/browser';
+import numeral from 'numeral';
+
 import 'typeface-poppins';
 import 'typeface-notosans-kor';
 import 'typeface-noto-sans';
@@ -6,8 +11,6 @@ import '@fortawesome/fontawesome-free/css/fontawesome.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import './src/scss/theme.scss';
-
-import numeral from 'numeral';
 
 numeral.register('locale', 'fs', {
   abbreviations: {
@@ -19,3 +22,43 @@ numeral.register('locale', 'fs', {
 });
 
 numeral.locale('fs');
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, eventId: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error });
+    Sentry.withScope(scope => {
+      scope.setExtras(errorInfo);
+      const eventId = Sentry.captureException(error);
+      this.setState({ eventId });
+    });
+  }
+
+  render() {
+    const { error, eventId } = this.state;
+    const { children } = this.props;
+    if (error) {
+      return (
+        <div
+          onClick={() => {}}
+          onKeyPress={() => Sentry.showReportDialog({ eventId })}
+          role="button"
+          tabIndex="0"
+        >
+          Report feedback
+        </div>
+      );
+    }
+    return children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.shape,
+};
+
+export default ErrorBoundary;
